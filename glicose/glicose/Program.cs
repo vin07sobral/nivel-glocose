b05e87b1f40f9a0ca56ec9bfdaedcb4c1d4e3d67
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:21:38 -0300

    Add files via upload

---
+++ glicose/glicose/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace revisao_logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double glicose;
+            glicose = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (glicose <= 100.0)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (glicose <= 140.0)
+            {
+                Console.WriteLine("Elevado");
+            }
+            else
+            {
+                Console.WriteLine("Diabetes");
+            }
+            Console.ReadLine();
+        }
+    }
+}
